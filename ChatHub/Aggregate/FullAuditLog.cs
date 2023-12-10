b2855ec0d26fd0ee594cfe5582b69dc62af81c2c
BirajMainali <birajmainali10@gmail.com>
2023-12-10 16:58:49 +0545

    feat: add audit log aggregate

---
+++ ChatHub/Aggregate/FullAuditLog.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatHub.Aggregate;
+
+public class FullAuditLog<T>
+{
+    public T Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Status Status { get; set; }
+    public string Remarks { get; set; }
+}
+
+public enum Status
+{
+    Active,
+    Pending,
+    Deleted,
+    Deactivated
+}
